634b15596a8ade6057affe02061f714bdb81d98d
geetpal <geetpal.gs@gmail.com>
2025-02-21 23:19:29 -0500

    Spotify auth and track search setup

---
+++ frontend/auth.ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth"
+import Spotify from "next-auth/providers/spotify"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Spotify],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      return session;
+    },
+    // async redirect({ url, baseUrl }) {
+    //   return `${baseUrl}/dashboard`;
+    // },
+  },
+})
